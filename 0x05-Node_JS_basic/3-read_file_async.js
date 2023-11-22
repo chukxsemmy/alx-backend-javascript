f1a50ab185cec119c483c411196d23ac29b4753a
chukxsemmy <chukxsemmy@gmail.com>
2023-11-21 19:22:38 -0800

    more complex HTTP server using Express

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,45 +3,62 @@
  * countStudents: counts the number of students in each category
  * with data read from a database file asynchronously.
  */
+
 const fs = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
-  return new Promise((resolve, reject) => {
-    readFile(fileName, (error, data) => {
+/**
+ * readData: reads the data and counts the
+ * number of students in each field.
+ * @param {list} data - list of students
+ */
+const readData = (data) => {
+  const logs = [];
+  const studentsArray = data.trim().split('\n').slice(1);
+
+  const studentInfo = `Number of students: ${studentsArray.length}`;
+  logs.push(studentInfo);
+  console.log(studentInfo);
+
+  const students = studentsArray.map((student) => {
+    const fields = student.replace('\r', '').split(',');
+    return fields;
+  });
+
+  const categories = [...new Set(students.map((student) => student[student.length - 1]))];
+
+  categories.forEach((category) => {
+    const filteredStudents = students.filter(
+      (student) => student[student.length - 1] === category,
+    ).map((student) => student[0]);
+
+    const info = `Number of students in ${category
+    }: ${filteredStudents.length}. List: ${filteredStudents.join(', ')}`;
+    logs.push(info);
+    console.log(info);
+  });
+
+  return logs;
+};
+
+/**
+ * countStudents - Reads the file asynchronously.
+ * @param {string} database - path to database file
+ * @returns promise
+ */
+const countStudents = (database) => {
+  const readFilePromise = new Promise((resolve, reject) => {
+    if (!database) {
+      reject(new Error('Cannot load the database'));
+    }
+    fs.readFile(database, 'utf8', (error, data) => {
       if (error) {
         reject(Error('Cannot load the database'));
       } else {
-        const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
-            } else {
-              fields[field[3]] = 1;
-            }
-          }
-        }
-        const l = length - 1;
-        console.log(`Number of students: ${l}`);
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-          }
-        }
-        resolve(data);
+        resolve(readData(data));
       }
     });
   });
-}
+  return readFilePromise;
+};
 
 module.exports = countStudents;
