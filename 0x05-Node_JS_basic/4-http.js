125093cc38c66f8de8fb2626fe8a74ccdbad1fc1
chukxsemmy <chukxsemmy@gmail.com>
2023-11-21 17:45:49 -0800

    Small http server using node http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+/**
+ * Small HTTP server using the http module.
+ */
+
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;
