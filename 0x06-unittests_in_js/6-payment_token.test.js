930ae7f6fe7ce431359c4ec57ea10f9c44999d1d
chukxsemmy <chukxsemmy@gmail.com>
2023-11-23 18:22:12 -0800

    Skip test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,17 +1,13 @@
-const sinon = require('sinon');
-const { expect } = require('chai');
-const { getPaymentTokenFromAPI } = require('./6-payment_token');
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('getPaymentTokenFromAPI', () => {
-  it('getPaymentTokenFromAPI', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((resp) => {
-        expect(resp.data).to.be.string;
-        expect(resp.data).to.equals('Successful response from the API');
-      });
-    done()
-      .catch((error) => {
-        done(error);
-      });
-  });
+describe("getPaymentTokenFromAPI", function() {
+    it("Async testing with done callback", function(done) {
+	getPaymentTokenFromAPI(true)
+	    .then((data) => {
+		expect(data).to.have.property('data');
+		done();
+	    });
+    });
 });
