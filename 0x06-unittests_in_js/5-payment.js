c4311b9c16373bc49eb1ca1390c383b31e6ce079
chukxsemmy <chukxsemmy@gmail.com>
2023-11-23 18:16:58 -0800

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,7 @@
+const Utils = require('./utils')
+function sendPaymentRequestToApi(totalAmount, totalShiping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShiping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
