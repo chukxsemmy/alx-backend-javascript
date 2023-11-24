770329f7324c7d1c8c9df3c8bdbeb8c82df55357
chukxsemmy <chukxsemmy@gmail.com>
2023-11-23 18:19:34 -0800

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { getPaymentTokenFromAPI } = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('getPaymentTokenFromAPI', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((resp) => {
+        expect(resp.data).to.be.string;
+        expect(resp.data).to.equals('Successful response from the API');
+      });
+    done()
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
